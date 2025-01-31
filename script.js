caab987e45586b8f0efff2e91f78f4e40dd80c62
Jessicasru <111495984+Jessicasru@users.noreply.github.com>
2025-01-31 21:18:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Select the button and the paragraph
+
 const button = document.getElementById("changeTextButton");
 const paragraph = document.querySelector("p");
 
-// Add a click event listener to the button
 button.addEventListener("click", function() {
     paragraph.textContent = "You clicked the button! 🎉";
 });
